445483b29cac812a95b1ebb20bb76d0e11e3e8b6
inchei <inchei@protonmail.com>
2026-07-05 21:03:52 +0800

    feat: select whether to show possibly missing links on subject pages

--- wikiMissingPositions/src/api.js
+++ wikiMissingPositions/src/api.js
@@ -1,20 +1,36 @@
-import { SETTINGS_KEY, DEFAULT_PROVIDER } from './config.js';
+import { PROVIDER_KEY, DEFAULT_PROVIDER, SHOW_KEY } from './config.js';
 
 function hasChiiApp() {
   return typeof chiiApp !== 'undefined' && chiiApp;
 }
 
-export function getProvider() {
+function get(key, def) {
   if (hasChiiApp()) {
-    return chiiApp.cloud_settings.get(SETTINGS_KEY) || DEFAULT_PROVIDER;
+    return chiiApp.cloud_settings.get(key) || def;
   }
-  return localStorage.getItem(SETTINGS_KEY) || DEFAULT_PROVIDER;
+  return localStorage.getItem(key) || def;
+}
+
+export function getProvider() {
+  return get(PROVIDER_KEY, DEFAULT_PROVIDER);
+}
+
+export function getShow() {
+  return get(SHOW_KEY, 'on');
+}
+
+function save(key, val) {
+  if (hasChiiApp()) {
+    chiiApp.cloud_settings.update({ [key]: val });
+    return;
+  }
+  localStorage.setItem(key, val);
 }
 
 export function saveProvider(val) {
-  if (hasChiiApp()) {
-    chiiApp.cloud_settings.update({ [SETTINGS_KEY]: val });
-    return;
-  }
-  localStorage.setItem(SETTINGS_KEY, val);
+  save(PROVIDER_KEY, val);
+}
+
+export function saveShow(val) {
+  save(SHOW_KEY, val);
 }
